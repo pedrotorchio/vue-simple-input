b9cb54e58516e57cb3cf7fa9c9547987181c89b0
pedrotorchio <pedrotorchio@gmail.com>
2018-02-06 13:14:23 -0300

    inicial

---
+++ SimpleInput.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  props: {
+    id:String, 
+    classString:String, 
+    required: {
+      type:Boolean,
+      default: false
+    },
+    suffix:String,
+    placeholder:String,
+    type:{
+      type: String,
+      default: 'text'
+    },
+    validator: {
+      type: Function,
+      default: null
+    },
+    validateWhen:{
+      type:Function,
+      default:model=>model.length
+    },
+    value:{
+      required: true
+    }
+  },
+  computed:{
+    props(){return {}},
+    model:{
+      get(){
+        return this.inputTransform(this.value);
+      },
+      set(value){
+        this.$emit('input', this.outputTransform(value));
+        if(this.validator)
+          this.$emit('isValid', this.validator(value)); 
+      }
+    },
+    validationColor(){
+      return this.isValid() ? 'success' : 'error';
+    }
+  },
+  methods:{
+    isValid(){
+      // se quiser validar sem transformação inputTransform, aplicar validator a this.value
+      if(this.validator)
+        return this.validator(this.model);
+
+      return false;
+    },
+    inputTransform(value){
+      return value;
+    },
+    outputTransform(value){
+      return value;
+    }
+  }
+}
+</script>
+<template src='./SimpleInput.htm'></template>
+<style lang='scss' src='./SimpleInput.scss' scoped></style>
