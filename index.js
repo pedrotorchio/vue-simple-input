1a8ebb306522287ad6c04d9bf2de3b116ab948a9
pedrotorchio <pedrotorchio@gmail.com>
2018-08-08 14:13:25 -0300

    exporting new inputs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 export {default as SimpleInput} from './SimpleInput.vue';
 export {default as SimpleMaskedInput} from './SimpleMaskedInput.vue';
 export {default as SimpleNumericInput} from './SimpleNumericInput.vue';
+export {default as SimpleSelectInput} from './SimpleSelectInput.vue';
+export {default as SimpleFileInput} from './SimpleFileInput.vue';
